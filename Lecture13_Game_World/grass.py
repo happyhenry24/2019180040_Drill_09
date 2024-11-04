68bb6d129d1a750148f7f130007b2a33cf0a6ca7
happyhenry24 <joyongcheon24@gmail.com>
2024-11-04 11:47:03 +0900

    Drill 09 제출 (2019180040 조용천)

---
+++ Lecture13_Game_World/grass.py
@@ -0,0 +1,22 @@
+from pico2d import load_image
+
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+    def update(self):
+        pass
+
+class Grass2:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 20)
+
+    def update(self):
+        pass
